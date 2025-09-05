2d334ae381202bc4bc0520b18c9b627032a37d15
gillor <lorenz.gillner@hs-wismar.de>
2025-09-05 15:55:47 +0200

    Fix promotion of numeric data types

--- src/combinations.jl
+++ src/combinations.jl
@@ -19,7 +19,11 @@ function combine_dempster(X::BPA, Y::BPA)
     # subtract once so we don't have to do it for every iteration below
     one_minus_K = 1 - sum(p[1].second * p[2].second for p in ps if isempty(p[1].first ∩ p[2].first))
 
-    r = BPA{first(eltype(X).types), last(eltype(X).types)}([
+    # equalize BPA data types
+    p_type = promote_type(first(eltype(X).types), first(eltype(Y).types))
+    e_type = promote_type(last(eltype(X).types), last(eltype(Y).types))
+
+    r = BPA{p_type, e_type}([
         e => sum(p[1].second * p[2].second for p in ps if (p[1].first ∩ p[2].first) == (e ∩ e); init = 0) / one_minus_K
         for e in es
     ])
